d6ed71b27d73a47dee403ac96088c01a5316f8a7
samxiao <490429806@qq.com>
2019-08-16 15:06:24 +0800

    v2ex

--- App/GetHot.go
+++ App/GetHot.go
@@ -53,27 +53,34 @@ func (spider Spider) GetV2EX() []map[string]interface{} {
 	var Body io.Reader
 	request, err := http.NewRequest("GET", url, Body)
 	if err != nil {
-		fmt.Println("抓取" + spider.DataType + "失败")
+		fmt.Println("抓取" + "失败")
 		return []map[string]interface{}{}
 	}
 	request.Header.Add("User-Agent", `Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Mobile Safari/537.36`)
 	res, err := client.Do(request)
 	if err != nil {
-		fmt.Println("抓取" + spider.DataType + "失败")
+		fmt.Println("抓取" + "失败")
 		return []map[string]interface{}{}
 	}
 	defer res.Body.Close()
 	document, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
-		fmt.Println("抓取" + spider.DataType + "失败")
+		fmt.Println("抓取" + "失败")
 		return []map[string]interface{}{}
 	}
 	var allData []map[string]interface{}
-	document.Find(".item_title").Each(func(i int, selection *goquery.Selection) {
-		url, boolUrl := selection.Find("a").Attr("href")
-		text := selection.Find("a").Text()
+	document.Find(".cell table").Each(func(i int, selection *goquery.Selection) {
+		category := selection.Find(".node").Text()
+		url, boolUrl := selection.Find(".item_title a").Attr("href")
+		text := selection.Find(".item_title a").Text()
+		count := selection.Find(".count_livid").Text()
+
 		if boolUrl {
-			allData = append(allData, map[string]interface{}{"title": text, "url": "https://www.v2ex.com" + url})
+			allData = append(allData, map[string]interface{}{"title": text,
+				"url":      "https://www.v2ex.com" + url,
+				"count":    count,
+				"category": category,
+			})
 		}
 	})
 	return allData
@@ -406,6 +413,7 @@ func (spider Spider) GetGitHub() []map[string]interface{} {
 			})
 		}
 	})
+
 	return allData
 }
 
--- App/main.go
+++ App/main.go
@@ -5,7 +5,6 @@ import (
 	"github.com/PuerkitoBio/goquery"
 	"io"
 	"net/http"
-	"strings"
 	"time"
 )
 
@@ -15,7 +14,7 @@ func main() {
 }
 
 func GetZhiHu() []map[string]interface{} {
-	url := "https://github.com/trending/php?since=daily"
+	url := "https://www.v2ex.com/?tab=hot"
 	timeout := time.Duration(5 * time.Second) //超时时间5s
 	client := &http.Client{
 		Timeout: timeout,
@@ -28,43 +27,28 @@ func GetZhiHu() []map[string]interface{} {
 	}
 	request.Header.Add("User-Agent", `Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Mobile Safari/537.36`)
 	res, err := client.Do(request)
-
 	if err != nil {
 		fmt.Println("抓取" + "失败")
 		return []map[string]interface{}{}
 	}
 	defer res.Body.Close()
-	var allData []map[string]interface{}
 	document, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
 		fmt.Println("抓取" + "失败")
 		return []map[string]interface{}{}
 	}
+	var allData []map[string]interface{}
+	document.Find(".cell table").Each(func(i int, selection *goquery.Selection) {
+		category := selection.Find(".node").Text()
+		url, boolUrl := selection.Find(".item_title a").Attr("href")
+		text := selection.Find(".item_title a").Text()
+		count := selection.Find(".count_livid").Text()
 
-	document.Find(".Box article").Each(func(i int, selection *goquery.Selection) {
-		s := selection.Find(".lh-condensed a")
-		url, boolUrl := s.Attr("href")
-		text := s.Text()
-		descText := selection.Find("p").Text()
-
-		category := selection.Find(".d-inline-block").AfterHtml("programmingLanguage").Find("span").Text()
-
-		var star, fork string
-
-		selection.Find(".muted-link").Each(func(i int, selection *goquery.Selection) {
-			if i == 0 {
-				star = selection.Text()
-			}
-			if i == 1 {
-				fork = selection.Text()
-			}
-		})
 		if boolUrl {
-			allData = append(allData, map[string]interface{}{"title": strings.Trim(text, "\r\n"), "desc":
-			descText, "url": "https://github.com" + url,
+			allData = append(allData, map[string]interface{}{"title": text,
+				"url":      "https://www.v2ex.com" + url,
+				"count":    count,
 				"category": category,
-				"star":     star,
-				"fork":     fork,
 			})
 		}
 	})
